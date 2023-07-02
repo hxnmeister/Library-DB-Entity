b8e8adcbf5da21bbfeeeaf3b05cf0484d0a937f2
Valerii <valeriy.stronskiy@gmail.com>
2023-07-02 16:41:21 +0300

    First part

---
+++ Домашнє завдання/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Домашнє_завдання
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LibraryTabControl = new System.Windows.Forms.TabControl();
+            this.AuthorTabPage = new System.Windows.Forms.TabPage();
+            this.ConfirmAuthorButton = new System.Windows.Forms.Button();
+            this.AuthorInfoTextBox = new System.Windows.Forms.TextBox();
+            this.AuthorComboBox = new System.Windows.Forms.ComboBox();
+            this.AddAuthorButton = new System.Windows.Forms.Button();
+            this.AuthorsDataGridView = new System.Windows.Forms.DataGridView();
+            this.BookTabPage = new System.Windows.Forms.TabPage();
+            this.PublisherTabPage = new System.Windows.Forms.TabPage();
+            this.DeleteAuthorButton = new System.Windows.Forms.Button();
+            this.EditAuthorButton = new System.Windows.Forms.Button();
+            this.LibraryTabControl.SuspendLayout();
+            this.AuthorTabPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AuthorsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LibraryTabControl
+            // 
+            this.LibraryTabControl.Controls.Add(this.AuthorTabPage);
+            this.LibraryTabControl.Controls.Add(this.BookTabPage);
+            this.LibraryTabControl.Controls.Add(this.PublisherTabPage);
+            this.LibraryTabControl.Location = new System.Drawing.Point(12, 12);
+            this.LibraryTabControl.Name = "LibraryTabControl";
+            this.LibraryTabControl.SelectedIndex = 0;
+            this.LibraryTabControl.Size = new System.Drawing.Size(776, 426);
+            this.LibraryTabControl.TabIndex = 0;
+            this.LibraryTabControl.SelectedIndexChanged += new System.EventHandler(this.LibraryTabControl_SelectedIndexChanged);
+            // 
+            // AuthorTabPage
+            // 
+            this.AuthorTabPage.Controls.Add(this.EditAuthorButton);
+            this.AuthorTabPage.Controls.Add(this.DeleteAuthorButton);
+            this.AuthorTabPage.Controls.Add(this.ConfirmAuthorButton);
+            this.AuthorTabPage.Controls.Add(this.AuthorInfoTextBox);
+            this.AuthorTabPage.Controls.Add(this.AuthorComboBox);
+            this.AuthorTabPage.Controls.Add(this.AddAuthorButton);
+            this.AuthorTabPage.Controls.Add(this.AuthorsDataGridView);
+            this.AuthorTabPage.Location = new System.Drawing.Point(4, 22);
+            this.AuthorTabPage.Name = "AuthorTabPage";
+            this.AuthorTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.AuthorTabPage.Size = new System.Drawing.Size(768, 400);
+            this.AuthorTabPage.TabIndex = 0;
+            this.AuthorTabPage.Text = "Authors";
+            this.AuthorTabPage.UseVisualStyleBackColor = true;
+            // 
+            // ConfirmAuthorButton
+            // 
+            this.ConfirmAuthorButton.Enabled = false;
+            this.ConfirmAuthorButton.Location = new System.Drawing.Point(634, 349);
+            this.ConfirmAuthorButton.Name = "ConfirmAuthorButton";
+            this.ConfirmAuthorButton.Size = new System.Drawing.Size(61, 45);
+            this.ConfirmAuthorButton.TabIndex = 4;
+            this.ConfirmAuthorButton.Text = "Confirm Entry";
+            this.ConfirmAuthorButton.UseVisualStyleBackColor = true;
+            this.ConfirmAuthorButton.Click += new System.EventHandler(this.ConfirmAuthorButton_Click);
+            // 
+            // AuthorInfoTextBox
+            // 
+            this.AuthorInfoTextBox.Enabled = false;
+            this.AuthorInfoTextBox.Location = new System.Drawing.Point(662, 285);
+            this.AuthorInfoTextBox.Name = "AuthorInfoTextBox";
+            this.AuthorInfoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AuthorInfoTextBox.TabIndex = 3;
+            this.AuthorInfoTextBox.Enter += new System.EventHandler(this.AuthorInfoTextBox_Enter);
+            // 
+            // AuthorComboBox
+            // 
+            this.AuthorComboBox.FormattingEnabled = true;
+            this.AuthorComboBox.Items.AddRange(new object[] {
+            "Id",
+            "FirstName",
+            "LastName"});
+            this.AuthorComboBox.Location = new System.Drawing.Point(634, 258);
+            this.AuthorComboBox.Name = "AuthorComboBox";
+            this.AuthorComboBox.Size = new System.Drawing.Size(128, 21);
+            this.AuthorComboBox.TabIndex = 2;
+            this.AuthorComboBox.SelectedIndexChanged += new System.EventHandler(this.AuthorComboBox_SelectedIndexChanged);
+            // 
+            // AddAuthorButton
+            // 
+            this.AddAuthorButton.Enabled = false;
+            this.AddAuthorButton.Location = new System.Drawing.Point(701, 349);
+            this.AddAuthorButton.Name = "AddAuthorButton";
+            this.AddAuthorButton.Size = new System.Drawing.Size(61, 45);
+            this.AddAuthorButton.TabIndex = 1;
+            this.AddAuthorButton.Text = "Add Value";
+            this.AddAuthorButton.UseVisualStyleBackColor = true;
+            this.AddAuthorButton.Click += new System.EventHandler(this.AddAuthorButton_Click);
+            // 
+            // AuthorsDataGridView
+            // 
+            this.AuthorsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.AuthorsDataGridView.Location = new System.Drawing.Point(6, 6);
+            this.AuthorsDataGridView.MultiSelect = false;
+            this.AuthorsDataGridView.Name = "AuthorsDataGridView";
+            this.AuthorsDataGridView.Size = new System.Drawing.Size(756, 246);
+            this.AuthorsDataGridView.TabIndex = 0;
+            // 
+            // BookTabPage
+            // 
+            this.BookTabPage.Location = new System.Drawing.Point(4, 22);
+            this.BookTabPage.Name = "BookTabPage";
+            this.BookTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.BookTabPage.Size = new System.Drawing.Size(768, 400);
+            this.BookTabPage.TabIndex = 1;
+            this.BookTabPage.Text = "Books";
+            this.BookTabPage.UseVisualStyleBackColor = true;
+            // 
+            // PublisherTabPage
+            // 
+            this.PublisherTabPage.Location = new System.Drawing.Point(4, 22);
+            this.PublisherTabPage.Name = "PublisherTabPage";
+            this.PublisherTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.PublisherTabPage.Size = new System.Drawing.Size(768, 400);
+            this.PublisherTabPage.TabIndex = 2;
+            this.PublisherTabPage.Text = "Publishers";
+            this.PublisherTabPage.UseVisualStyleBackColor = true;
+            // 
+            // DeleteAuthorButton
+            // 
+            this.DeleteAuthorButton.Location = new System.Drawing.Point(102, 258);
+            this.DeleteAuthorButton.Name = "DeleteAuthorButton";
+            this.DeleteAuthorButton.Size = new System.Drawing.Size(90, 45);
+            this.DeleteAuthorButton.TabIndex = 5;
+            this.DeleteAuthorButton.Text = "Delete Selected Value";
+            this.DeleteAuthorButton.UseVisualStyleBackColor = true;
+            this.DeleteAuthorButton.Click += new System.EventHandler(this.DeleteAuthorButton_Click);
+            // 
+            // EditAuthorButton
+            // 
+            this.EditAuthorButton.Location = new System.Drawing.Point(6, 258);
+            this.EditAuthorButton.Name = "EditAuthorButton";
+            this.EditAuthorButton.Size = new System.Drawing.Size(90, 45);
+            this.EditAuthorButton.TabIndex = 6;
+            this.EditAuthorButton.Text = "Confirm Editing  Value";
+            this.EditAuthorButton.UseVisualStyleBackColor = true;
+            this.EditAuthorButton.Click += new System.EventHandler(this.EditAuthorButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LibraryTabControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.LibraryTabControl.ResumeLayout(false);
+            this.AuthorTabPage.ResumeLayout(false);
+            this.AuthorTabPage.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AuthorsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl LibraryTabControl;
+        private System.Windows.Forms.TabPage AuthorTabPage;
+        private System.Windows.Forms.TabPage BookTabPage;
+        private System.Windows.Forms.TabPage PublisherTabPage;
+        private System.Windows.Forms.DataGridView AuthorsDataGridView;
+        private System.Windows.Forms.Button ConfirmAuthorButton;
+        private System.Windows.Forms.TextBox AuthorInfoTextBox;
+        private System.Windows.Forms.ComboBox AuthorComboBox;
+        private System.Windows.Forms.Button AddAuthorButton;
+        private System.Windows.Forms.Button EditAuthorButton;
+        private System.Windows.Forms.Button DeleteAuthorButton;
+    }
+}
+
---
+++ Домашнє завдання/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Домашнє_завдання
+{
+    public partial class Form1 : Form
+    {
+        myLibraryEntities myLibrary = null;
+        Author author = null;
+        Book book = null;
+        Publisher publisher = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            myLibrary = new myLibraryEntities();
+            author = new Author();
+            book = new Book();
+            publisher = new Publisher();
+
+            AuthorsDataGridView.DataSource = myLibrary.Author.ToList();
+        }
+
+        private bool CheckNumber(string value, out int result)
+        {
+            return Int32.TryParse(value, out result);
+        }
+        private void SuccsessMessage(string value)
+        {
+            MessageBox.Show($"Значення {value} було записано!");
+        }
+        
+        private void LibraryTabControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(LibraryTabControl.SelectedIndex)
+            { 
+                case 0:
+                    AuthorsDataGridView.DataSource = myLibrary.Author.ToList();
+                    author = new Author();
+                    break;
+            }
+        }
+        private void AuthorComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(!AuthorInfoTextBox.Enabled) AuthorInfoTextBox.Enabled = true;
+            if(!ConfirmAuthorButton.Enabled) ConfirmAuthorButton.Enabled = true;
+        }
+
+        private void ConfirmAuthorButton_Click(object sender, EventArgs e)
+        {
+            switch (AuthorComboBox.SelectedIndex)
+            {
+                case 0:
+                    if (CheckNumber(AuthorInfoTextBox.Text, out int number) && !myLibrary.Author.ToList().Any(x => x.Id == number))
+                    {
+                        author.Id = number;
+                        SuccsessMessage(number.ToString());
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введено не коректне значення Id!");
+                    }
+                    break;
+                case 1:
+                    author.FirstName = AuthorInfoTextBox.Text;
+                    SuccsessMessage(AuthorInfoTextBox.Text);
+                    break;
+                case 2:
+                    author.LastName = AuthorInfoTextBox.Text;
+                    SuccsessMessage(AuthorInfoTextBox.Text);
+                    break;
+            }
+
+            AuthorInfoTextBox.Focus();
+            if (author.Id != 0 && author.LastName != null && author.FirstName != null) AddAuthorButton.Enabled = true;
+        }
+
+        private void AuthorInfoTextBox_Enter(object sender, EventArgs e)
+        {
+            AuthorInfoTextBox.Clear();
+        }
+
+        private void AddAuthorButton_Click(object sender, EventArgs e)
+        {
+            myLibrary.Author.Add(author);
+            myLibrary.SaveChanges();
+
+            AuthorInfoTextBox.Enabled = false;
+            ConfirmAuthorButton.Enabled = false;
+            AuthorComboBox.SelectedIndex = -1;
+            AddAuthorButton.Enabled = false;
+
+            MessageBox.Show("Нового автора було додано, оновіть вкладку щоб побачити зміни!");
+
+            author = new Author();
+        }
+
+        private void DeleteAuthorButton_Click(object sender, EventArgs e)
+        {
+            myLibrary.Author.Remove(AuthorsDataGridView.SelectedRows[0].DataBoundItem as Author);
+
+            myLibrary.SaveChanges();
+
+            MessageBox.Show("Для того щоб побачити зміни оновіть вкладку!");
+        }
+
+        private void EditAuthorButton_Click(object sender, EventArgs e)
+        {
+            AuthorsDataGridView.CellValueChanged += AuthorsDataGridView_CellValueChanged;
+        }
+
+        private void AuthorsDataGridView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+    }
+}
